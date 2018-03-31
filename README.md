a58cde98421d97dc9144e0d2939f08d4c82a6f4b
mynameisvinn <vin.tang@gmail.com>
2018-03-31 08:02:01 -0400

    clean up

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 sprite is an mapreduce implementation with aws lambda. inspired by [david hayden](https://davidhayden.org/articles/create-python-function-on-aws-lambda-using-serverless/).
 
 ## why serverless?
-classic mapreduce with hadoop/spark/dask means touching infrastructure code. 
+classic mapreduce with hadoop/spark/dask means touching infrastructure code. with Sprite, we dont have to think about server provisioning, cluster management, resource management, scheduling. in fact, we wont need to know *anything* about infrastructure.
 
-with lambdas, we dont have to think about server provisioning, cluster management, resource management, scheduling. in fact, we wont need to know *anything* about infrastructure.
+## example
+```python
+v = Sprite("mynameisvinn", access="hello", secret="secret")
+v.map(np.sum, np.arange(100), n_chunks=10)
+# returns [45, 145, 245, 345, 445, 545, 645, 745, 845, 945]
+```
---
+++ Sprite.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pickle
+import boto3
+import time
+
+class Sprite(object):
+    def __init__(self, bucket, access, secret):
+        self.bucket = bucket
+        self.client = self.create_client(access, secret)
+        
+    def create_client(self, a, s):
+        return boto3.client('s3', aws_access_key_id=a, aws_secret_access_key=s)
+    
+    def map(self, func, data, n_chunks):
+        # serialize and put function in s3 (vinn-dumps)
+        serialized_func = pickle.dumps(func)
+        self.client.put_object(Body=serialized_func, Bucket="vinn-dump", Key="pickled_function")
+        
+        # create and save data chunks to s3 (mynameisvinn), where it's processed and saved to vinn-dump
+        chunks = np.split(data, n_chunks)
+        for i, c in enumerate(chunks):
+            _ = self._put(i, c)
+            
+        # step 3: fetch results from vinn-dump
+        time.sleep(3)
+        res = []
+        for i in range(n_chunks):
+            fname = str(i)
+            obj = self.client.get_object(Bucket="vinn-dump", Key=fname)
+            pickled_function = obj['Body'].read()
+            d = pickle.loads(pickled_function)
+            res.append(d)
+        
+        return res
+    
+    def _put(self, uid, chunk):
+        pickled_data = pickle.dumps(chunk)
+        self.client.put_object(Body=pickled_data, Bucket=self.bucket, Key=str(uid))
